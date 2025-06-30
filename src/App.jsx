3c34efd61991047b0e1b8779024f163151f62dc2
NiravDevre <devrenirav1@gmail.com>
2025-06-30 09:41:34 +0530

    cocktail section

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { ScrollTrigger , SplitText } from "gsap/all";
 
 import Navbar from "./components/Navbar.jsx"
 import Hero from "./components/Hero.jsx"
-
+import Cocktails from "./components/Cocktails.jsx";
 gsap.registerPlugin(ScrollTrigger, SplitText);
+
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
-      <div className="h-dvh bg-black"></div>
+      <Cocktails />
     </main>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 import gsap from "gsap";
+import ScrollTrigger from 'gsap/ScrollTrigger';
 import { navLinks } from "../../constants/index.js";
 import { useGSAP } from "@gsap/react";
+gsap.registerPlugin(ScrollTrigger);
 
 const Navbar = () => {
-    useGSAP(() => {
-        const navTween = gsap.timeline({
-            scrollTrigger: {
-                trigger: 'nav',
-                start: 'bottom top'
-            }
-        });
-        navTween.fromTo('nav', {backgroundColor: 'transparent'},{
-            backgroundColor: '#00000050', 
-            backgroundFilter: 'blur(10px)',
-            duration: 1,
-            ease: "power1.inOut"
-        });
+  useGSAP(() => {
+    const nav = document.querySelector('nav');
 
-    })
+    ScrollTrigger.create({
+      trigger: nav,
+      start: 'top+=5 top',
+      end: 'bottom top',
+      onEnter: () => {
+        gsap.to(nav, {
+          backgroundColor: 'rgba(0, 0, 0, 0.3)',
+          backdropFilter: 'blur(10px)',
+          duration: 1,
+          ease: 'power1.inOut',
+        });
+      },
+      onLeaveBack: () => {
+        gsap.to(nav, {
+          backgroundColor: 'transparent',
+          backdropFilter: 'blur(0px)',
+          duration: 1,
+          ease: 'power1.out'
+        });
+      }
+    });
+  }, []);
     return(
         <nav>
             <div>
